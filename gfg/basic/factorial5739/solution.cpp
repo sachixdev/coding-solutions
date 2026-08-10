3ee06be7f5690d289f35415811a1706e626a580d
Sachi Somankar <somankarsachi@gmail.com>
2026-08-10 14:09:43 +0530

    ✅ Factorial | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ gfg/basic/factorial5739/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int factorial(int n) {
+        // code here
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+        
+    }
+};
