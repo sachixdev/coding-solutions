6498f68a50fbd9e07714ed40ffbbcd685799ca0c
Sachi Somankar <somankarsachi@gmail.com>
2026-08-10 14:23:54 +0530

    ✅ Nth Fibonacci Number | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-10

---
+++ gfg/easy/nth-fibonacci-number1335/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int nthFibonacci(int n) {
+        // code here
+        if(n==0 ){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        for(int i=1 ; i<=n ; i++){
+            return nthFibonacci(n-1)+nthFibonacci(n-2);
+        }
+    }
+};
