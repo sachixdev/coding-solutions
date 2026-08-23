84d258719adb123433cc483a55983e175b86583c
Sachi Somankar <somankarsachi@gmail.com>
2026-08-24 00:41:48 +0530

    ✅ Spy Number | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ gfg/basic/spy-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool checkSpy(int n) {
+        int sum = 0;
+        int product = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit;
+            product *= digit;
+            n /= 10;
+        }
+
+        return sum == product;
+    }
+};
