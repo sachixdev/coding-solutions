04e0e9dca1070d5dc0946412b742c50826fa63c0
Sachi Somankar <somankarsachi@gmail.com>
2026-09-08 16:11:15 +0530

    ✅ Count Set Bits | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ gfg/easy/set-bits0143/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int setBits(int n) {
+        int count = 0;
+
+        while (n) {
+            n = n & (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+};
